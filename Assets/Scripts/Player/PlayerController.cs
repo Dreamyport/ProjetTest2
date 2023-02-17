d12b73cf9f79f5ce8e938b54051f9cab11649630
Dreamyport <thedreamyport@hotmail.com>
2023-02-17 09:18:15 -0500

    Changes.
    Added a ground and a player movement script.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    private void Start()
+    {
+        // Position initiale du joueur.
+        transform.position = new Vector3(-12f, 0.5f, -12f);
+    }
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float xPos = Input.GetAxis("Horizontal");
+        float zPos = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(xPos, 0f, zPos);
+        transform.Translate(direction * Time.deltaTime * moveSpeed);
+    }
+}
