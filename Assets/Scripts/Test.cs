55d72ac9e679aced5c9e70af6304f5318a2d8105
Dreamyport <thedreamyport@hotmail.com>
2023-02-17 08:13:19 -0500

    Initial commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
